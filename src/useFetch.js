798ce2bba802794723a530ee8a70a1b3efb13777
Andy Nguyen <139949316+andyn-network@users.noreply.github.com>
2023-08-14 11:51:28 +1000

    Update useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,42 +1,51 @@
 import { useState, useEffect } from "react";
 
+// Custom hook for fetching data from a given URL
 const useFetch = (url) => {
+    // State variables to store data, loading status, and errors
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
-        // runs on every render/refresh
+    // useEffect hook to fetch data when the component mounts or the URL changes
     useEffect(() => {
+        // Create an instance of AbortController to handle aborting the fetch
         const abortCont = new AbortController();
 
+        // Delayed fetch using setTimeout to simulate loading
         setTimeout(() => {
+            // Fetch data from the provided URL using the AbortController signal
             fetch(url, { signal: abortCont.signal })
                 .then(res => {
-                    if(!res.ok) {
-                        throw Error('could not fetch the data for that resource');
+                    // Check if the response is not okay, then throw an error
+                    if (!res.ok) {
+                        throw Error('Could not fetch the data for that resource');
                     }
-                    return res.json();
+                    return res.json(); // Parse response data as JSON
                 })
                 .then((data) => {
+                    // Update state with fetched data and set loading and error states
                     setData(data);
                     setIsPending(false);
                     setError(null);
                 })
                 .catch(err => {
-                    if(err.name === 'AbortError') {
-                        console.log('fetch aborted')
+                    if (err.name === 'AbortError') {
+                        console.log('Fetch aborted');
                     } else {
+                        // Update loading state and set the error message
                         setIsPending(false);
                         setError(err.message);
                     }
-                })
-        }, 200);
+                });
+        }, 200); // Simulated delay
 
+        // Cleanup function to abort the fetch if the component unmounts or URL changes
         return () => abortCont.abort();
-    }, [url]); 
-    // empty array [] only runs the function once after the first inital render
-    
-    return { data, isPending, error }
-}
+    }, [url]); // Dependency array, re-run effect if the URL changes
 
-export default useFetch;
+    // Return the data, loading status, and error for the component using this hook
+    return { data, isPending, error };
+};
+
+export default useFetch; // Export the custom hook for use in other components
